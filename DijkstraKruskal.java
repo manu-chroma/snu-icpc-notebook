c835b8dbcc3ff1e0575d4608790dc083cb6926a5
Bhishmaraj <bs386@snu.edu.in>
2016-12-15 02:31:04 +0530

    added comments and ETF (phi) in PrimeUtils

--- DijkstraKruskal.java
+++ DijkstraKruskal.java
@@ -1,66 +1,51 @@
-	static boolean marked[]; // Part of dijkstra
-	static long distTo[];
-	static ArrayList<Edge>[] adj;
+static class Edge implements Comparable<Edge> {
+	int u , v;
+	long cost;
+	Edge(int u , int v, long cost) {
+		this.u = u;
+		this.v = v;
+		this.cost = cost;
 
-	static class Edge implements Comparable<Edge> {
-		int v;
-		long cost;
+	}
+	Edge(int v , long cost) {
+		this(-1 , v , cost);
+	}
+	@Override
+	public int compareTo(Edge o) {
+		return Long.compare(this.cost, o.cost);
+	}
+}
 
-		Edge(int v, long cost) {
-			this.v = v;
-			this.cost = cost;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Long.compare(this.cost, o.cost);
+static long[] dijkstra(int start , int V) {  // Dependency adj (adjacency list of type Edge) , vertex count V 
+	PriorityQueue<Edge> pq = new PriorityQueue<>();
+	pq.add(new Edge(start, 0));
+	boolean marked[] = new boolean[V + 1]; 
+	long distTo[] = new long[V + 1];
+	while (!pq.isEmpty()) {
+		Edge min = pq.remove();
+		int u = min.v;
+		if(!marked[u]){
+			marked[u] = true;
+			distTo[u] = min.cost;
+			for (Edge e : adj[u])
+				if (!marked[e.v])
+					pq.add(new Edge(e.v, e.cost + distTo[u]));
 		}
 	}
 
-	static void dijkstra(int start) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(start, 0));
+	return distTo;
+}
 
-		while (!pq.isEmpty()) {
-			Edge min = pq.remove();
-			int u = min.v;
-			if(!marked[u]){
-				marked[u] = true;
-				distTo[u] = min.cost;
-				for (Edge e : adj[u])
-					if (!marked[e.v])
-						pq.add(new Edge(e.v, e.cost + distTo[u]));
-			}
+static long KruskalMST(Edge arr[], int V) { // Dependency : array of edges (Type Edge) , vertex count V , DisjointSetUnion data structure
+	Arrays.sort(arr);
+	DisjointSetUnion dsu = new DisjointSetUnion(V);
+	long minCost = 0;
+	for (int i = 0; i < arr.length && dsu.components() != 1; i++) {
+		if (!dsu.connected(arr[i].u, arr[i].v)) {
+			dsu.union(arr[i].u, arr[i].v);
+			minCost += arr[i].cost;
 		}
 	}
-
-	static class Edge_MST implements Comparable<Edge_MST> // For kruskal MST
-	{
-		int u, v;
-		long cost;
-
-		Edge_MST(int u, int v, long cost) {
-			this.u = u;
-			this.v = v;
-			this.cost = cost;
-		}
-
-		@Override
-		public int compareTo(Edge_MST o) {
-			return Long.compare(this.cost, o.cost);
-		}
-	}
-
-	static long KruskalMST(Edge_MST arr[], int V) {
-		Arrays.sort(arr);
-		DSU dsu = new DSU(V);
-		long minCost = 0;
-		for (int i = 0; i < arr.length && dsu.components() != 1; i++) {
-			if (!dsu.connected(arr[i].u, arr[i].v)) {
-				dsu.union(arr[i].u, arr[i].v);
-				minCost += arr[i].cost;
-			}
-		}
-		return minCost;
-	}
+	return minCost;
+}
 
--- PrimeUtils.java
+++ PrimeUtils.java
@@ -1,45 +1,85 @@
-public static boolean[] isPrimeArray(int N) // Sieve of Erathanoses
-{
-	boolean num[] = new boolean[N + 1];
-	Arrays.fill(num, true);
-	num[1] = num[0]=  false;
-	for (int i = 2; i * i <= N; i++)
-		if (num[i])  // i is prime
-			for (int j = i * i; j <= N; j += i)
-				num[j] = false;
+public static boolean[] isPrimeArray(int N) { // Sieve of Erathanoses
+boolean num[] = new boolean[N + 1];
+Arrays.fill(num, true);
+num[1] = num[0]=  false;
+for (int i = 2; i * i <= N; i++)
+	if (num[i])  // i is prime
+		for (int j = i * i; j <= N; j += i)
+			num[j] = false;
 
 
-	return num;
+return num;
 }
 
-public static int[] sieve(int N) // Sieve of Erathanoses dependency : isPrimeArray()
-{
+public static int[] sieve(int N) { // Sieve of Erathanoses dependency : isPrimeArray()
 
-	boolean isPrime[] = isPrimeArray(N);
-	int sz = 0;
-	for(boolean b : isPrime)
-		sz += b ? 1 : 0;
-	int arr[] = new int[sz];
-	int ptr = 0;
-	for (int i = 2; i <= N; i++)
-		if (isPrime[i])
-			arr[ptr++] = i;
+boolean isPrime[] = isPrimeArray(N);
+int sz = 0;
+for(boolean b : isPrime)
+	sz += b ? 1 : 0;
+int arr[] = new int[sz];
+int ptr = 0;
+for (int i = 2; i <= N; i++)
+	if (isPrime[i])
+		arr[ptr++] = i;
 
-	return arr;
+return arr;
 }
 
 static BitSet segmentedSieve(long L , long R) {  // Dependency : Primes till sqrt(R) 
-	int len = (int) (R - L + 1);
-	BitSet bitSet = new BitSet(len);
-	bitSet.set(0, len);
-	for(long p : primes)
-		for(long i = L % p == 0 ? L : L + (p - (L % p));i <= R;i += p)
-			if(i != p)
-				bitSet.set((int) (i - L) , false);
-	
-	if(L == 1)
-		bitSet.set(0, false);
-	
-	return bitSet;
+int len = (int) (R - L + 1);
+BitSet bitSet = new BitSet(len);
+bitSet.set(0, len);
+for(long p : primes)
+	for(long i = L % p == 0 ? L : L + (p - (L % p));i <= R;i += p)
+		if(i != p)
+			bitSet.set((int) (i - L) , false);
+
+if(L == 1)
+	bitSet.set(0, false);
+
+return bitSet;
 }
 static int primes[];
+
+static void loPrimeSieve(int N) { // stores lowest prime factor of each number used for calculating phi
+	loPrime = new int[N + 1];
+	int pr[] = new int[N];
+	int sz = 0;
+	for (int i = 2; i <= N; ++i) {
+		if (loPrime[i] == 0) {
+			loPrime[i] = i;
+			pr[sz] = i;
+			sz++;
+		}
+		for (int j = 0; j < sz && pr[j] <= loPrime[i] && i * pr[j] <= N; ++j)
+			loPrime[i * pr[j]] = pr[j];
+	}
+}
+
+static int lp[];
+static int fi[]; // euler totient function
+
+static void fastFiSieve(int N) {
+	lp = new int[N + 1];
+	fi = new int[N + 1];
+	int pr[] = new int[N];
+	int sz = 0;
+	fi[1] = 1;
+	for (int i = 2; i <= N; ++i) {
+		if (lp[i] == 0) {
+			lp[i] = i;
+			fi[i] = i - 1;
+			pr[sz] = i;
+			sz++;
+		} else {
+			// Calculating phi
+			if (lp[i] == lp[i / lp[i]])
+				fi[i] = fi[i / lp[i]] * lp[i];
+			else
+				fi[i] = fi[i / lp[i]] * (lp[i] - 1);
+		}
+		for (int j = 0; j < sz && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+			lp[i * pr[j]] = pr[j];
+	}
+}
