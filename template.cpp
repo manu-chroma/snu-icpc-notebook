1b3b91440ad5f9dd9ee66a5e9055cf51efa2969e
Manvendra Singh <manvendra0310@gmail.com>
2016-12-19 13:55:11 +0530

    updated c++ starter template

--- template.cpp
+++ template.cpp
@@ -1,10 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define pb push_back
+#define mp make_pair
+
 typedef long long int li;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-  
-  return 0;
+const int INF = 0x3f3f3f3f; //infinity
+
+int main()
+{	
+	// not to be used when using scanf/printf
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    
+    return 0;
 }
